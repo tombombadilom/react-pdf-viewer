7fbade014427528570dd1c0ac66322b9a671592d
TomBombadilom <tom@croll.fr>
2024-03-15 22:40:24 +0100

    build in mjs

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -14,28 +14,59 @@ const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.pee
 
 const plugins = [json(), typescript()];
 
+// module.exports = [
+//     // CJS
+//     {
+//         input,
+//         output: {
+//             exports: 'named',
+//             file: path.join(outputDir, `cjs/${pgkName}.js`),
+//             format: 'cjs',
+//         },
+//         external,
+//         plugins,
+//     },
+
+//     // Minified CJS
+//     {
+//         input,
+//         output: {
+//             exports: 'named',
+//             file: path.join(outputDir, `cjs/${pgkName}.min.js`),
+//             format: 'cjs',
+//         },
+//         external,
+//         plugins: plugins.concat([terser()]),
+//     },
+// ];
+// ...
 module.exports = [
-    // CJS
+    // CJS configurations...
+    
+    // Add an ESM bundle configuration
     {
         input,
         output: {
-            exports: 'named',
-            file: path.join(outputDir, `cjs/${pgkName}.js`),
-            format: 'cjs',
+            dir: path.join(outputDir, 'esm'), // Utilisez `dir` pour écrire dans un répertoire si vous générez plusieurs morceaux
+            entryFileNames: `${pgkName}.js`, // Nom du fichier pour l'entrée principale
+            format: 'es', // Format ES module
+            sourcemap: true // Optionnel, si vous voulez des source maps
         },
         external,
         plugins,
     },
 
-    // Minified CJS
+    // Minified ESM
     {
         input,
         output: {
-            exports: 'named',
-            file: path.join(outputDir, `cjs/${pgkName}.min.js`),
-            format: 'cjs',
+            dir: path.join(outputDir, 'esm'),
+            entryFileNames: `${pgkName}.min.js`,
+            format: 'es',
+            sourcemap: true
         },
         external,
-        plugins: plugins.concat([terser()]),
+        plugins: plugins.concat([terser()]), // Concaténer terser plugin pour la minification
     },
 ];
+
